7189b4c65ed0ed7cdf84f968db0570a2308087d0
wonhee.lee <lwh0102@gmail.com>
2024-06-24 00:20:23 +0900

    initial commit

---
+++ src/test/java/com/socoolheeya/travel/yaml/importer/TravelYamlImporterApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.socoolheeya.travel.yaml.importer;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class TravelYamlImporterApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
